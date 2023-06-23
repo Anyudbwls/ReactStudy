fbf35e86f386f53f194efbcc12ad6d2905795fe1
Yujin <jyj15938@gmail.com>
2023-06-23 15:21:54 +0900

    recucer 상태관리

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,47 +1,34 @@
-import React, { useState } from 'react';
-
+import React, { useReducer } from 'react';
+import personReducer from './reducer/person-reducer';
 export default function AppMentors() {
-  const [formState, setFromState] = useState(initialPerson);
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶으신가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶으신가요?`);
-    setFromState((person) => ({
-      ...person,
-      mentors: formState.mentors.map((mentor) => {
-        if (mentor.name === prev) {
-          return { ...mentor, name: current };
-        }
-        return mentor;
-      }),
-    }));
+    dispatch({ type: 'updated', prev, current });
   };
   const handleAdd = () => {
-    const currentName = prompt(`추가하고 싶은 멘토의 이름은 무엇인가요?`);
-    const currentTitle = prompt(`추가하고싶은 멘토의 직업은 무엇인가요?`);
+    const name = prompt(`추가하고 싶은 멘토의 이름은 무엇인가요?`);
+    const title = prompt(`추가하고싶은 멘토의 직업은 무엇인가요?`);
 
-    setFromState((person) => ({
-      ...person,
-      mentors: [...person.mentors, { currentName, currentTitle }],
-    }));
+    dispatch({ type: 'added', name, title });
   };
 
   const handleDelete = () => {
-    const currentName = prompt(`삭제하고 싶은 멘토의 이름은 무엇인가요?`);
+    const name = prompt(`삭제하고 싶은 멘토의 이름은 무엇인가요?`);
 
-    setFromState((person) => ({
-      ...person,
-      mentors: formState.mentors.filter((m) => m.name !== currentName),
-    }));
+    dispatch({ type: 'updated', name });
   };
 
   return (
     <div>
       <div>
-        {formState.name}은 {formState.age}이고 {formState.title}이다.
+        {person.name}은 {person.age}이고 {person.title}이다.
       </div>
 
       <ul>
-        {formState.mentors.map((mentor, index) => (
+        {person.mentors.map((mentor, index) => (
           <li key={index}>
             {mentor.name}({mentor.title})
           </li>
---
+++ src/reducer/person-reducer.js
@@ -0,0 +1,34 @@
+/* eslint-disable default-case */
+export default function personReducer(person, action) {
+  switch (action.type) {
+    case 'updated': {
+      const { prev, current } = action;
+      return {
+        ...person,
+        mentors: person.mentors.map((mentor) => {
+          if (mentor.name === prev) {
+            return { ...mentor, name: current };
+          }
+          return mentor;
+        }),
+      };
+    }
+    case 'added': {
+      const { name, title } = action;
+      return {
+        ...person,
+        mentor: [...person.mentors, { name, title }],
+      };
+    }
+
+    case 'deleted': {
+      return {
+        ...person,
+        mentor: person.mentors.filter((mentor) => mentor.name !== action.name),
+      };
+    }
+    default: {
+      throw Error(`알수없는 액션 타입이다 : ${action.type}`);
+    }
+  }
+}
